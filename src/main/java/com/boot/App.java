9f5a3391b3068b1996c5d25e86d4a64993b68bab
Bruno Querino <bquerino@gmail.com>
2017-09-20 23:00:53 -0300

    src update

--- src/main/java/com/boot/App.java
+++ src/main/java/com/boot/App.java
@@ -1,7 +1,10 @@
 package com.boot;
 
+import org.h2.server.web.WebServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
 
 /**
  * Hello world!
@@ -14,4 +17,13 @@ public class App
     {
         SpringApplication.run(App.class, args);
     }
+
+    
+    /*    
+    @Bean
+    public ServletRegistrationBean h2servletRegistration() {
+        ServletRegistrationBean registration = new ServletRegistrationBean(new WebServlet());
+        registration.addUrlMappings("/h2/*");
+        return registration;
+    }*/
 }
